8eb367bdd25ab347f126a3c3b0a56f8500b38532
Peter Szabo <szwazzup@gmail.com>
2022-04-09 09:40:00 +0200

    elmélet

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -8,16 +8,44 @@ public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
         Ide jön a válaszod...
+
+        Egész szám típusú primitív adattípusok: byte, short, int long
+        Törtszám típusú primitív adattípusok: float, double
+        Nem számtípusú primitív adattípusok: char, boolean
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
         Ide jön a válaszod...
+
+        A while előltesztelő ciklus, a do-while pedig hátul tesztelő ciklus.
+
+        A while-t akkor használom, ha nem tudom, hogy hányszor kell lefuttatni, hogy a feltétel teljesüljön.
+        A do-whilet-t akkor használom, ha nem tudom pontosan, hogy hányszor fusson le, de egyszer tutira le szeretném
+        futtatni.
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
         Ide jön a válaszod...
+
+        Rendszerint tömböket használunk adattárolására, de adatgyűjtésre is kiváló. :-)
+
+        A tömbök tárolóobjektum, amelyben tárolhatunk:
+            - egyszerű típusú adatokat,
+            - értékek,
+            - öszetett típusú objektumokat, akár több dimenzióban is.
+
+       A tömb indexelése 0-val kezdődik.
+
+        A tömb maga is egy objektum.
+        Tömb létrehozásainak módjai:
+            int[] tomb = new int[10];
+            int[0] tomb = 1; == int tomb[0] = 1;
+            int[] tomb2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            String[][] tomb3 = new String[2][2];
+            String[] tomb4 = {"alma", "korte", "barack"};
+
      */
 
 }
